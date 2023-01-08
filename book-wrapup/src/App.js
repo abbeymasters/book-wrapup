03064754d6a0ae29a8b7e81f8904beed38f25909
Abbey Masters <abbeykmasters@gmail.com>
2023-01-07 23:13:25 -0800

    added footer styling

--- book-wrapup/src/App.js
+++ book-wrapup/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from "./Header"
+import Footer from "./Footer"
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
--- book-wrapup/src/Header.js
+++ book-wrapup/src/Header.js
@@ -3,12 +3,10 @@ import "./Header.css"
 
 const Header = () => {
     return(
-        <>
-            <section>
-                <h1>ABBEY & ALLISON'S</h1>
-                <h2>Top 5 Books of 2022</h2>    
-            </section>
-        </>
+        <section>
+            <h1>ABBEY & ALLISON'S</h1>
+            <h2>Top 5 Books of 2022</h2>    
+        </section>
     )
 }
 
