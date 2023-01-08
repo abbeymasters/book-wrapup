c56cf373080a08222b6553f65868abb25834a423
Abbey Masters <abbeykmasters@gmail.com>
2023-01-07 22:16:11 -0800

    header added

---
+++ book-wrapup/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Header.css"
+
+const Header = () => {
+    return(
+        <>
+            <section>
+                <h1>ABBEY & ALLISON'S</h1>
+                <h2>Top 5 Books of 2022</h2>    
+            </section>
+        </>
+    )
+}
+
+export default Header;
